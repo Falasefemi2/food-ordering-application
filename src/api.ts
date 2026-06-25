5378f902689ebd987fe8b5894c3c3f86fc54839e
falase femi <femifalase228@gmail.com>
2026-06-26 00:58:02 +0100

    feat: image uploading with cloudinary

--- src/api.ts
+++ src/api.ts
@@ -2,10 +2,12 @@ import { HttpApi, OpenApi } from "effect/unstable/httpapi";
 import { AuthApiGroup } from "./auth/auth-api";
 import { RestaurantApiGroup } from "./restaurant/restaurant-api";
 import { AdminApiGroup } from "./admin/admin-api";
+import { RestaurantUploadApiGroup } from "./restaurant/restaurant-upload-api";
 
 export class Api extends HttpApi.make("chowdeck-api")
 	.add(AuthApiGroup)
 	.add(RestaurantApiGroup)
+	.add(RestaurantUploadApiGroup)
 	.add(AdminApiGroup)
 	.annotateMerge(
 		OpenApi.annotations({
